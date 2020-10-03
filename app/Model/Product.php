367621a4d376a114cb37f733c70c37af6dbe0085
ddeshar <jedeshar@gmail.com>
2020-10-03 20:18:30 +0700

    added update on controller with request

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -5,8 +5,12 @@ namespace App\Model;
 use App\Model\Review;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
-{
+class Product extends Model{
+
+    protected $fillable = [
+        'name','detail','stock','price','discount'
+    ];
+
     public function reviews(){
         return $this->hasMany(Review::class);
     }
